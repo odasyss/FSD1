94514cdaa291cc775f83938b3633d424e1d2b145
odasys <71523344+odasyss@users.noreply.github.com>
2021-06-09 00:20:00 -0700

    Add files via upload

--- 104/ToDoList/Todo.js
+++ 104/ToDoList/Todo.js
@@ -1,20 +1,20 @@
+//click on trashd to delete 
 $('ul').on('click','span',function(event){
-    $(this).parent().fadeOut(500,function(){ //fadeOut on li
-      $(this).remove(); //all removed
-    });
-    event.stopPropagation();
-  })
-  $('ul').on('mouseover','span',function(){
-    this.style.display;
-  }).on('click','span',function(){
-    this.style.display = "none";
-  })
-  
-  $("input[type='text']").on('keypress',function(enter_key){
-    if(enter_key.which === 10) {
-      var text = $(this).val();
-      $(this).val("");
-      $('ul').append("<li><span><i class='fas fa-trash-alt'></i> </span> "+text+"</li>");//adds to html
-    }
-  })
-  
+  $(this).parent().fadeOut(500,function(){ 
+    $(this).remove(); // remove
+  });
+  event.stopPropagation();
+})
+$('ul').on('mouseover','span',function(){
+  this.style.display;
+}).on('click','span',function(){
+  this.style.display = "none";
+})
+
+$("input[type='text']").on('keypress',function(enter_key){
+  if(enter_key.which === 13){
+    var text = $(this).val();
+    $(this).val("");
+    $('ul').append("<li><span><i class='fas fa-trash-alt'></i> </span> "+text+"</li>");
+  }
+})
