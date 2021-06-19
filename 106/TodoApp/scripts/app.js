08013d4e4e8a0bd370abc60b5d19a3e985d71607
odasys <71523344+odasyss@users.noreply.github.com>
2021-06-18 22:53:00 -0700

    Add files via upload

---
+++ 106/TodoApp/scripts/app.js
@@ -0,0 +1,41 @@
+var count = 0;
+// index=0;
+
+function saveTodo() {
+    let text = $("#todoTxt").val();
+    //validations
+    if (!text) {
+        alert("Information missing..")
+        return; 
+    }
+    count++;
+    updateCounter();
+    let update = "<div class='todo-item'>" + text + " <button id='del' class='btn btn-sm btn-danger'> Delete </button> </div>"
+    
+    $("#todoContainer").append(update);
+    //clear text
+    $("#todoTxt").val("");
+    $("#todoTxt").focus();
+}
+
+function updateCounter(){
+    let counter = "<div class='counter-task'> To Do's: " + count + "</div>";
+    $("#tasks").html(counter);
+}
+
+function init() {
+    $("#btnSave").click(saveTodo);
+    $("#todoTxt").keypress(function (args) {
+        if (args.key == "Enter") {
+            saveTodo();
+        }
+    });
+    $("#todoContainer").on('click', '.btn-danger', function () {
+        let div = $(this).parent();
+        div.remove();
+        count--;
+        updateCounter();
+    });
+}
+
+window.onload = init();
